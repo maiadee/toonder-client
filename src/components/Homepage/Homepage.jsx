b2fb8a104b3d829ad1cca79ee3bdfad8db2cab1d
Anna Jones <annamariajones@live.co.uk>
2025-02-12 15:57:48 +0100

    homepage css

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,21 +1,24 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../contexts/UserContext";
+import styles from "./homepage.module.css";
 
 export default function Homepage() {
   const { user } = useContext(UserContext);
   const navigate = useNavigate();
 
   return (
-    <div>
-      <img src="/images/logo.png" alt="Toonder Logo" />
-
-      {!user && (
-        <>
-          <p>Sign Up To Find Love Today! 💕</p>
-          <button onClick={() => navigate("/signup")}>Sign Up</button>
-        </>
-      )}
-    </div>
+    <div className={styles.homepage}>
+      <img className="logo" src="/images/ToonderWhite.png" alt="Toonder Logo" />
+<div className={styles.content}>
+        {!user && (
+          <>
+             {/* <p>Sign Up To Find Love Today! 💕</p> */}
+            <button onClick={() => navigate("/signup")}>Sign Up</button> 
+          </>
+        )}
+        </div>
+      </div>
   );
 }
+
