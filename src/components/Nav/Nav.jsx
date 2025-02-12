b3037a55883c42932dcf3b52f65c04246d464c16
Anna Jones <annamariajones@live.co.uk>
2025-02-12 10:45:36 +0100

    navbar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,29 +1,50 @@
 import { useState, useContext } from 'react'
-import { NavLink } from 'react-router'
-import Button from 'react-bootstrap/Button'
-import Offcanvas from 'react-bootstrap/Offcanvas'
-import styles from './NavMenu.module.css'
+import { NavLink } from 'react-router-dom'  
 import { UserContext } from '../../contexts/UserContext'
 import { removeToken } from '../../utils/auth'
 
 export default function NavMenu() {
-    
     const { user, setUser } = useContext(UserContext)
-  
-  
-    const [show, setShow] = useState(false)
-  
-    const handleClose = () => setShow(false)
-    const handleShow = () => setShow(true)
-  
+
+    const [menuOpen, setMenuOpen] = useState(false)
+
+    const toggleMenu = () => setMenuOpen(!menuOpen)
+
     const signOut = () => {
-      
-      removeToken()
-     
-      setUser(null)
+        removeToken()
+        setUser(null)
+        setMenuOpen(false)
     }
 
+    return (
+        <nav>
+            
+            <button onClick={toggleMenu}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </button>
 
 
-
+            {menuOpen && (
+                <div>
+                    <nav>
+                        <NavLink to="/" onClick={() => setMenuOpen(false)}>Home</NavLink>
+                        {user ? (
+                            <>
+                                <NavLink to="/matches" onClick={() => setMenuOpen(false)}>View Matches</NavLink>
+                                <NavLink to="/profile" onClick={() => setMenuOpen(false)}>Update Profile</NavLink>
+                                <button onClick={signOut}>Sign Out</button>
+                            </>
+                        ) : (
+                            <>
+                                <NavLink to="/signup" onClick={() => setMenuOpen(false)}>Sign Up</NavLink>
+                                <NavLink to="/login" onClick={() => setMenuOpen(false)}>Log In</NavLink>
+                            </>
+                        )}
+                    </nav>
+                </div>
+            )}
+        </nav>
+    )
 }
